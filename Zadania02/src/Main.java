d27f5dcce034bbf3f9c9dfcc11e16f106b299b76
Maciej Strzalka <Mstrzalka2008@gmail.com>
2026-09-15 11:22:06 +0200

    wykonano zadanie sprawdzające możliwość zrobienia trójkąta

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -10,4 +10,19 @@ void main() {
     } else {
         System.out.println("Liczba nie jest podzielna przez 3.");
     }
+
+    System.out.print("Podaj pierwszy bok: ");
+    double a = scanner.nextDouble();
+    System.out.print("Podaj drugi bok: ");
+    double b = scanner.nextDouble();
+    System.out.print("Podaj trzeci bok: ");
+    double c = scanner.nextDouble();
+
+    if (a + b > c && a + c > b && b + c > a) {
+        System.out.println("Z podanych boków można zbudować trójkąt.");
+    } else {
+        System.out.println("Z podanych boków nie można zbudować trójkąta.");
+    }
+
+    
 }
